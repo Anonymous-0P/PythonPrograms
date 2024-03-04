d228727d9f244924e7b0649623634e81991b9b76
Prakash Karekar <120655334+Anonymous-0P@users.noreply.github.com>
2024-03-04 19:50:48 +0530

    Update 4A.py

--- program 4A/4A.py
+++ program 4A/4A.py
@@ -11,3 +11,36 @@ arr = [int(x) for x in arr]
 print("Original list:", arr)
 insertion_sort(arr)
 print("Sorted list:", arr)
+
+#merge sort 
+def merge_sort(arr):
+ if len(arr) > 1:
+ mid = len(arr) // 2
+ left = arr[:mid]
+ right = arr[mid:]
+ merge_sort(left)
+ merge_sort(right)
+Dept. of CSE, SDMIT, Ujire 15
+ PYTHON PROGRAMMING LABORATORY (21CSL46)
+ i = j = k = 0
+ while i < len(left) and j < len(right):
+ if left[i] < right[j]:
+ arr[k] = left[i]
+ i += 1
+ else:
+ arr[k] = right[j]
+ j += 1
+ k += 1
+ while i < len(left):
+ arr[k] = left[i]
+ i += 1
+ k += 1
+ while j < len(right):
+ arr[k] = right[j]
+ j += 1
+ k += 1
+arr = input("Enter a list of numbers separated by commas: ").split(",")
+arr = [int(x) for x in arr]
+print("Original list:", arr)
+merge_sort(arr)
+print("Sorted list:", arr)
